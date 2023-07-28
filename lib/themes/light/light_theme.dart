d9d7d94e1d6f46b8f57a350643d61fb6b50861ad
shakhawat-khan <satanfromhelll666@gmail.com>
2023-07-28 17:43:25 +0600

    light_theme

---
+++ lib/themes/light/light_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../themes.dart';
+
+ThemeData get lightTheme => ThemeData(
+      // primarySwatch: Colors.indigo,
+      //useMaterial3: true,
+      elevatedButtonTheme: elevatedButtonTheme,
+      inputDecorationTheme: textFieldTheme,
+    );
